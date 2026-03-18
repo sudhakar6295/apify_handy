9edf4d2902c3d6eb23afc1a4329e81e63ec976fa
sudhakar6295 <53999168+sudhakar6295@users.noreply.github.com>
2026-03-18 06:40:33 +0530

    Update main.py

--- my_actor/main.py
+++ my_actor/main.py
@@ -23,7 +23,7 @@ START_URLS = [
 ]
 
 
-user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36",
+user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36"
 extra_http_headers = {
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
     "accept-language": "de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7",
